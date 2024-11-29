8cbc49361269e4dbd8797692918e7d3e5625bac1
のん <swanlake1532@gmail.com>
2024-11-29 19:12:49 +0900

    ファイル構造の整理

--- KAIMEMO/Extension/UIViewController.swift
+++ KAIMEMO/Extension/UIViewController.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    func setDismissKeyboard() {
+        let tapGR: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGR.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tapGR)
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+}
